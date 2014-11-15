d2b928be3e8184bffa78fcfeffe2c384c3b84c16
Sergey Alirzaev <zl29ah@gmail.com>
2014-11-15 22:06:58 +0300

    [slow]

--- muesli.hs
+++ muesli.hs
@@ -21,7 +21,7 @@ pills = map (* 100)
 
 -- grams per 100g
 -- nutrients:		[prote,	fat,	carbs,	fiber,
--- elem:potass,	sodium,	calciu,	magnes,	phosph,	iron,	iodine,	zinc,	seleni,	copper,	chromi,	mangane,molybden,chloride,fluoride,
+-- elem:potass,	sodium,	calciu,	magnes,	phosph,	iron,	iodine,	zinc,	seleni,	copper,	chromi,	mangane,molybde,chlorid,fluoride,
 -- vita:a,	c,	d,	e,	k,	thiami,	ribofl,	niacin,	pantot,	b6,	biotin,	folate,	b12,	omega3,	omega6,
 -- e aa:His,	Ile,	Leu,	Lys,	Met,	Phe,	Thr,	Trp,	Val,
 -- o aa:Ala,	Arg,	Asn,	Asp,	Cys,	Glu,	Gln,	Gly,	Orn,	Pro,	Sel,	Ser,	Tyr,
@@ -34,29 +34,39 @@ raisins = ck		[3,	0.5,	79,	5,	-- USDA 2446
 sunflowerKernel = ck	[19,	53,	23,	11,	-- USDA 3670
 	0.608,	0.24,	0.071,	0.128,	1.2,	3.7e-3,	0,	5e-3,	53e-6,	1.8e-3,	idk,	1.95e-3,idk,	idk,	todo,
 	0,	0,	0,	25e-3,	0,	1.48e-3,355e-6,	8.33e-3,1.13e-3,1.34e-3,idk,	227e-6,	0,	14e-3,	12,	-- from other sources
-	6.32e-1,	1.14e0,	1.66e0,	9.37e-1,	9.28e-1,1.17e0,	9.28e-1,	3.48e-1,	1.32e0,	1.12e0,	2.40e0,idk,	2.45e0,	4.51e-1,	5.58e0,	idk,	1.46e0,	idk,	1.18e0,	idk,	1.08e0,	6.66e-1,
+	6.32e-1,1.14e0,	1.66e0,	9.37e-1,9.28e-1,1.17e0,	9.28e-1,3.48e-1,1.32e0,	1.12e0,	2.40e0,	idk,	2.45e0,	4.51e-1,5.58e0,	idk,	1.46e0,	idk,	1.18e0,	idk,	1.08e0,	6.66e-1,
 	6.8]	-- auchan
+sunflowerOil = ck	-- USDA 650
+			[0.00e0,1.00e2,	0.00e0,	0.00e0,
+	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	idk,	0.00e0,	0.00e0,	0.00e0,	idk,	idk,	idk,	idk,	idk,
+	0.00e0,	idk,	0.00e0,	4.11e-2,5.40e-6,0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	idk,	0.00e0,	0,	0.00e0,	50,
+	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	0.00e0,	idk,	0.00e0,	0.00e0,	0.00e0,	idk,	0.00e0,	idk,	0.00e0,	idk,	0.00e0,	0.00e0,
+	4]	-- auchan
+brazilNuts = ck	-- USDA 3685
+	[1.43e1,	6.64e1,	1.23e1,	7.50e0,	6.59e-1,	3.00e-3,	1.60e-1,3.76e-1,	7.25e-1,	2.43e-3,	idk,	4.06e-3,	1.92e-3,1.74e-3,	idk,	1.22e-3,	idk,	idk,	idk,	0.00e0,	idk,	0.00e0,	5.73e-3,	0.00e0,	6.17e-4,	3.50e-5,	2.95e-4,	1.84e-4,	1.01e-4,	idk,	2.20e-5,	0.00e0,	1.70e-2,	1.70e-2,3.86e-1,	5.16e-1,	1.16e0,	4.92e-1,	3.62e-1,	6.30e-1,3.62e-1,	1.41e-1,	7.56e-1,	5.77e-1,	2.15e0,	idk,	1.35e0,	3.67e-1,	3.15e0,	idk,	7.18e-1,	idk,	6.57e-1,	idk,	6.83e-1,	4.20e-1,
+	64]	-- auchan
 oat = ck	-- USDA 6386
 			[1.69e1,	6.90e0,	6.63e1,
 	1.06e1,	4.29e-1,2.00e-3,5.40e-2,1.77e-1,5.23e-1,	4.72e-3,	idk,	3.97e-3,	idk,	6.26e-4,	idk,	4.92e-3,idk,	idk,	idk,	0.00e0,	idk,	0.00e0,	idk,	idk,	7.63e-4,	1.39e-4,	9.61e-4,	1.35e-3,	1.19e-4,idk,	5.60e-5,	0.00e0,	idk,	idk,	4.05e-1,	6.94e-1,	1.28e0,	7.01e-1,	5.75e-1,	8.95e-1,	5.75e-1,	2.34e-1,	9.37e-1,	8.81e-1,	1.19e0,idk,	1.45e0,	4.08e-1,	3.71e0,	idk,	8.41e-1,	idk,	9.34e-1,	idk,	7.50e-1,	5.73e-1,
 	2.7]	-- auchan
-parsleyDried = ck	[2.66e1,5.48e0,	5.06e1,	2.67e1,	-- USDA 254
-	2.68e0,	4.52e-1,1.14e0,	4.00e-1,4.36e-1,2.20e-2,idk,	5.44e-3,1.41e-5,7.80e-4,idk,	9.81e-3,idk,	idk,	idk,
-	9.70e-5,idk,	0.00e0,	8.96e-3,1.36e-3,1.96e-4,2.38e-3,9.94e-3,1.06e-3,9.00e-4,idk,	1.80e-4,0,	1.86,	1.264,	-- questionable
-	7.18e-1,	1.55e0,	2.79e0,	2.10e0,	1.19e0,	1.71e0,1.19e0,	4.75e-1,	2.02e0,	1.78e0,	1.76e0,	idk,	3.17e0,2.98e-1,	3.69e0,	idk,	1.76e0,	idk,	2.01e0,	idk,	1.16e0,1.16e0,
-	44]	-- auchan, 10g packs
+buckwheat = ck	-- USDA 6358
+			[1.32e1,	3.40e0,	7.15e1,	1.00e1,	4.60e-1,	1.00e-3,	1.80e-2,	2.31e-1,	3.47e-1,	2.20e-3,	idk,	2.40e-3,	8.30e-6,	1.10e-3,	idk,	1.30e-3,	idk,	idk,	idk,	0.00e0,	idk,	0.00e0,	idk,	idk,	1.01e-4,	4.25e-4,	7.02e-3,	1.23e-3,	2.10e-4,	idk,	3.00e-5,	0.00e0,	0.00e0,	idk,	3.09e-1,	4.98e-1,	8.32e-1,	6.72e-1,	5.06e-1,	5.20e-1,	5.06e-1,	1.92e-1,	6.78e-1,	7.48e-1,	9.82e-1,	idk,	1.13e0,	2.29e-1,	2.05e0,	idk,	1.03e0,	idk,5.07e-1,	idk,	6.85e-1,	2.41e-1,
+	5]
+rice = ck	-- USDA 6504
+			[6.61e0,	5.80e-1,	7.93e1,	idk,	8.60e-2,	1.00e-3,	9.00e-3,	3.50e-2,	1.08e-1,	8.00e-4,	idk,	1.16e-3,	idk,	1.10e-4,	idk,	1.10e-3,	idk,	idk,	idk,	idk,	idk,	0.00e0,	idk,	idk,	7.00e-5,	4.80e-5,	1.60e-3,	1.34e-3,	1.45e-4,	idk,	9.00e-6,	0.00e0,	idk,	idk,	1.55e-1,	2.85e-1,	5.46e-1,	2.39e-1,	2.36e-1,	3.53e-1,	2.36e-1,	7.70e-2,	4.03e-1,	3.83e-1,	5.51e-1,	idk,	6.21e-1,	1.35e-1,	1.29e0,	idk,	3.01e-1,	idk,	3.11e-1,	idk,	3.47e-1,	2.21e-1,
+	0]
+millet = ck	-- USDA 6379
+			[1.10e1,	4.22e0,	7.28e1,	8.50e0,	1.95e-1,	5.00e-3,	8.00e-3,	1.14e-1,	2.85e-1,	3.01e-3,	idk,	1.68e-3,	2.70e-6,	7.50e-4,	idk,	1.63e-3,	idk,	idk,	idk,	0.00e0,	idk,	0.00e0,	5.00e-5,	9.00e-7,	4.21e-4,	2.90e-4,	4.72e-3,	8.48e-4,	3.84e-4,	idk,	8.50e-5,	0.00e0,	idk,	idk,2.36e-1,	4.65e-1,	1.40e0,	2.12e-1,	3.53e-1,	5.80e-1,	3.53e-1,	1.19e-1,	5.78e-1,	9.86e-1,	3.82e-1,	idk,	7.26e-1,	2.12e-1,	2.40e0,	idk,	2.87e-1,	idk,	8.77e-1,	idk,	6.44e-1,	3.40e-1, 0]
 parsley = ck		[2.97e0,7.90e-1,6.33e0,	3.30e0,	-- USDA 3092
 	5.54e-1,5.60e-2,1.38e-1,5.00e-2,5.80e-2,6.20e-3,idk,	1.07e-3,1.00e-7,1.49e-4,idk,	1.60e-4,idk,	idk,	idk,
 	4.21e-4,idk,	0.00e0,	7.50e-4,1.64e-3,8.60e-5,9.80e-5,1.31e-3,4.00e-4,9.00e-5,idk,	1.52e-4,0.00e0,	8e-3,	118e-3,	-- http://nutritiondata.self.com/facts/vegetables-and-vegetable-products/2513/2
 	6.10e-2,	1.18e-1,	2.04e-1,	1.81e-1,	1.22e-1,	1.45e-1,	1.22e-1,	4.50e-2,1.72e-1,	1.95e-1,	1.22e-1,	idk,	2.94e-1,1.40e-2,	2.49e-1,	idk,	1.45e-1,	idk,	2.13e-1,	idk,	1.36e-1,	8.20e-2,
 	40]	-- local market
-flaxseedOil = ck	-- USDA 8275
-			[1.10e-1,1.00e2,0.00e0,	0.00e0,
-	0.00e0,	0.00e0,	1.00e-3,0.00e0,	1.00e-3,0.00e0,	idk,	7.00e-5,0.00e0,	0.00e0,	idk,	0.00e0,	idk,	idk,	idk,
-	0.00e0,	idk,	0.00e0,	4.70e-4,9.30e-6,0.00e0,	0.00e0,	0.00e0,	idk,	0.00e0,	idk,	0.00e0,	0.00e0,	5.34e1,	1.43e1,
-	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,
-	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,
-	22.4]	-- auchan, 500ml bottles
+parsleyDried = ck	[2.66e1,5.48e0,	5.06e1,	2.67e1,	-- USDA 254
+	2.68e0,	4.52e-1,1.14e0,	4.00e-1,4.36e-1,2.20e-2,idk,	5.44e-3,1.41e-5,7.80e-4,idk,	9.81e-3,idk,	idk,	idk,
+	9.70e-5,idk,	0.00e0,	8.96e-3,1.36e-3,1.96e-4,2.38e-3,9.94e-3,1.06e-3,9.00e-4,idk,	1.80e-4,0,	1.86,	1.264,	-- questionable
+	7.18e-1,	1.55e0,	2.79e0,	2.10e0,	1.19e0,	1.71e0,1.19e0,	4.75e-1,	2.02e0,	1.78e0,	1.76e0,	idk,	3.17e0,2.98e-1,	3.69e0,	idk,	1.76e0,	idk,	2.01e0,	idk,	1.16e0,1.16e0,
+	44]	-- auchan, 10g packs
 flaxseed = ck	-- USDA 3757, modified using data from 8275
 			[1.83e1,4.22e1,	2.89e1,	2.73e1,
 	8.13e-1,3.00e-2,2.55e-1,3.92e-1,6.42e-1,5.73e-3,idk,	4.34e-3,2.54e-5,1.22e-3,idk,	2.48e-3,idk,	idk,	idk,
@@ -64,6 +74,13 @@ flaxseed = ck	-- USDA 3757, modified using data from 8275
 	4.72e-1,8.96e-1,1.24e0,	8.62e-1,7.66e-1,9.57e-1,7.66e-1,2.97e-1,1.07e0,
 	9.25e-1,1.92e0,	idk,	2.05e0,3.40e-1,	4.04e0,	idk,	1.25e0,	idk,	8.06e-1,idk,	9.70e-1,4.93e-1,
 	14.75]	-- http://www.vetlek.ru/shop/?gid=20493&id=5964, 2kg packs
+flaxseedOil = ck	-- USDA 8275
+			[1.10e-1,1.00e2,0.00e0,	0.00e0,
+	0.00e0,	0.00e0,	1.00e-3,0.00e0,	1.00e-3,0.00e0,	idk,	7.00e-5,0.00e0,	0.00e0,	idk,	0.00e0,	idk,	idk,	idk,
+	0.00e0,	idk,	0.00e0,	4.70e-4,9.30e-6,0.00e0,	0.00e0,	0.00e0,	idk,	0.00e0,	idk,	0.00e0,	0.00e0,	5.34e1,	1.43e1,
+	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,
+	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,	idk,
+	22.4]	-- auchan, 500ml bottles
 
 naCl = ck		[0,	0,	0,	0,
 	0,	39.32,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	60.68,	0,
@@ -151,24 +168,35 @@ iomdriul = ck		[nan,	nan,	nan,	nan,
 -- 	[(grams, product)]	-- supplements
 -- 	)
 plantM = [
-		(0.58, oat),
+		(0.64, oat),
+		(0.22, raisins),
+		(0.02, parsleyDried),
+		(0.10, sunflowerOil),
+		(0.02, flaxseedOil)
+	]
+plantNoGlutenM = [
+		(0.58, buckwheat),
 		(0.19, raisins),
 		(0.19, sunflowerKernel),
 		(0.02, parsleyDried),
 		(0.02, flaxseed)
 	]
+seleniumS = (7, brazilNuts)
 electrolytesS = [
 		(3, naClI),
 		(3, kCl),
 		(2, caCl)
 	]
 completeR = (plantM, electrolytesS ++ [
+		seleniumS,
 		(0.1, ascorbicAcid),
 		(0.024, vigantol), -- one drop
 		(0.024, retinol) -- one drop
 	])
 simpleR = (plantM, electrolytesS ++ [
-		(1, aerovit) -- one pill
+		seleniumS,
+		(1, aerovit), -- one pill
+		(0.024, vigantol) -- one drop
 	])
 
 mix rec = map sum $ transpose $ map (\(frac, l) -> map (frac *) l) $ fst rec
