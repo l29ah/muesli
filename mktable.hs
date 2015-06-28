b3d22e14bee1ff31d11677ec2c8ad514372d19da
Sergey Alirzaev <zl29ah@gmail.com>
2015-06-28 03:21:04 +0300

    account for choline

--- mktable.hs
+++ mktable.hs
@@ -1,5 +1,4 @@
 #!/usr/bin/runhaskell
--- Usage: curl -s 'http://ndb.nal.usda.gov/ndb/foods/show/6454?format=Full&reportfmt=csv' | iconv -f cp1251 | ./mktable.hs
 import Data.List
 import Text.CSV
 
@@ -17,9 +16,15 @@ tablify :: [String] -> String
 tablify (name:unit:amount:_) = amount ++ utoe unit
 tablify [] = "idk"
 
-components = ["Protein", "Total lipid (fat)", "Carbohydrate, by difference", "Fiber, total dietary", "Potassium, K", "Sodium, Na", "Calcium, Ca", "Magnesium, Mg", "Phosphorus, P", "Iron, Fe", "Iodine, I", "Zinc, Zn", "Selenium, Se", "Copper, Cu", "Chromium, Cr", "Manganese, Mn", "Molybden, Md", "Chloride, Cl", "Fluoride, F", "Vitamin A, RAE", "Vitamin C, total ascorbic acid", "Vitamin D (D2 + D3)", "Vitamin E (alpha-tocopherol)", "Vitamin K (phylloquinone)", "Thiamin", "Riboflavin", "Niacin", "Pantothenic acid", "Vitamin B-6", "Biotin", "Folate, total", "Vitamin B-12", "18:3 n-3 c,c,c (ALA)", "18:2 n-6 c,c", "Histidine", "Isoleucine", "Leucine", "Lysine", "Threonine", "Phenylalanine", "Threonine", "Tryptophan", "Valine", "Alanine", "Arginine", "Asparagine", "Aspartic acid", "Cystine", "Glutamic acid", "Glutamine", "Glycine", "Ornithine", "Proline", "Selenocysteine", "Serine", "Tyrosine"]
+components = ["Protein", "Total lipid (fat)", "Carbohydrate, by difference", "Fiber, total dietary",
+	"Potassium, K", "Sodium, Na", "Calcium, Ca", "Magnesium, Mg", "Phosphorus, P", "Iron, Fe", "Iodine, I", "Zinc, Zn", "Selenium, Se", "Copper, Cu", "Chromium, Cr", "Manganese, Mn", "Molybden, Md", "Chloride, Cl", "Fluoride, F",
+	"Vitamin A, RAE", "Vitamin C, total ascorbic acid", "Vitamin D (D2 + D3)", "Vitamin E (alpha-tocopherol)", "Vitamin K (phylloquinone)", "Thiamin", "Riboflavin", "Niacin", "Pantothenic acid", "Vitamin B-6", "Biotin", "Folate, total", "Vitamin B-12", "Choline, total", "18:3 n-3 c,c,c (ALA)", "18:2 n-6 c,c",
+	"Histidine", "Isoleucine", "Leucine", "Lysine", "Threonine", "Phenylalanine", "Threonine", "Tryptophan", "Valine", "Alanine", "Arginine", "Asparagine", "Aspartic acid", "Cystine", "Glutamic acid", "Glutamine", "Glycine", "Ornithine", "Proline", "Selenocysteine", "Serine", "Tyrosine"]
 
 main = do
 	c <- getContents
-	let (Right cp) = parseCSV "" c
-	putStrLn $ intercalate ",\t" $ map (\c -> tablify $ llookup c cp) components
+	let cp = parseCSV "" c
+	either
+		print
+		(\r -> putStrLn $ intercalate ",\t" $ map (\c -> tablify $ llookup c r) components)
+		cp
